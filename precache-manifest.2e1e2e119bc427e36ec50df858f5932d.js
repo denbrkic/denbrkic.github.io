5c965d1f6c2494595305d2ed84e4d937d8a1ff2a
denbrkic <denbrkic@gmail.com>
2020-08-28 21:42:37 +0200

    demo 2

---
+++ precache-manifest.2e1e2e119bc427e36ec50df858f5932d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aaba2a4c278d35d9d34d60e11a7d567",
+    "url": "/index.html"
+  },
+  {
+    "revision": "919b34207b40d613d335",
+    "url": "/static/css/main.b547d02a.chunk.css"
+  },
+  {
+    "revision": "5558dfd0163007a6d0a7",
+    "url": "/static/js/2.76f5c989.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.76f5c989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919b34207b40d613d335",
+    "url": "/static/js/main.62b2febc.chunk.js"
+  },
+  {
+    "revision": "42636840ce84c142a936",
+    "url": "/static/js/runtime-main.04167da7.js"
+  }
+]);
